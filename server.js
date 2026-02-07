9920cb4d17208d40c3a51377b66939568efe7f03
GG889988 <1701961588@qq,com>
2026-02-08 00:25:21 +0800

    彻底修复：把错误的文件夹换成正确的文件

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('牛马得闲后端服务器运行正常！');
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`服务器运行在端口 ${port}`);
+});
